5274545400ad7e1289ea738352cd9eff0766136e
Georg Marek (4AHEL) <georg.marek@edu.htl-klu.at>
2024-07-03 09:54:38 +0200

    added working thingie

--- testing/src/test.java
+++ testing/src/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.pritln("craaazy feature. WOW"); //warum nix funken?
+        System.out.println("craaazy feature. WOW"); //funkt
+        System.out.println("mehr funktionen, jaaa");
     }
 }
